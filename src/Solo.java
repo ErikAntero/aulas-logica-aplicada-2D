73ae45c263503023aa05b58524106c83d3a94d9c
Erik Antero <erikanteromelo@hotmail.com>
2023-06-22 12:28:49 -0300

    modificaçoes 700000.0

---
+++ src/Solo.java
@@ -0,0 +1,10 @@
+public class Solo {
+
+    String tipoSolo;
+    boolean fertil;
+    String cor;
+    //método
+    public Planta gerarPlanta(Semente semente){
+        return new Planta (semente);
+    }
+}
